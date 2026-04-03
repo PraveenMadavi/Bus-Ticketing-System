036565e2afc096eb26042109ae089c62eb9983e1
PraveenMadavi <madavione@gmail.com>
2026-04-03 23:58:24 +0530

    first creation

---
+++ auth-service/auth-service/src/test/java/com/client/busticket/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.busticket.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ bus-service/bus-service/src/test/java/com/client/busticket/bus_service/BusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.busticket.bus_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
