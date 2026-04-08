5a1980e887f49ac14539e1b8f2fddcae326d75a0
PraveenMadavi <madavione@gmail.com>
2026-04-09 00:00:28 +0530

    security config

---
+++ auth-service/src/main/java/com/client/busticket/auth_service/configuration/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.client.busticket.auth_service.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager(); // Expose the AuthenticationManager bean
+    }
+}
