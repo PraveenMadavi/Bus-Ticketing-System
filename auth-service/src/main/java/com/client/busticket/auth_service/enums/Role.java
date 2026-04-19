8898b7988423d0750974a5658e071918b620ed9e
PraveenMadavi <madavione@gmail.com>
2026-04-19 23:59:49 +0530

    role edited

--- auth-service/src/main/java/com/client/busticket/auth_service/enums/Role.java
+++ auth-service/src/main/java/com/client/busticket/auth_service/enums/Role.java
@@ -1,7 +1,14 @@
 package com.client.busticket.auth_service.enums;
 
+import java.util.Set;
+
 public enum Role {
-    USER,
-    ADMIN,
-    OPERATOR
+    USER(Set.of(Permissions.APP_READ,Permissions.APP_WRITE)),
+    ADMIN(Set.of(Permissions.APP_READ)),
+    OPERATOR;
+    private final Set<Permissions> permissions;
+
+    Role(Set<Permissions> permissions) {
+        this.permissions = permissions;
+    }
 }
