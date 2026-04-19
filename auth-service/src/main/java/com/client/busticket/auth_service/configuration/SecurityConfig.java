7de5b078dcad7d91912190eaa1ab87d33de82909
PraveenMadavi <madavione@gmail.com>
2026-04-20 02:20:42 +0530

    PreAuths_anno_added.

--- auth-service/src/main/java/com/client/busticket/auth_service/configuration/SecurityConfig.java
+++ auth-service/src/main/java/com/client/busticket/auth_service/configuration/SecurityConfig.java
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +17,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final CustomUserDetailsService userDetailsService;
@@ -35,9 +39,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/operator/**").hasRole("OPERATOR")
+//                        .requestMatchers("/auth/**").permitAll()
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+//                        .requestMatchers("/operator/**").hasRole("OPERATOR")
                         .anyRequest().authenticated()
                 ).build();
     }
--- auth-service/src/main/java/com/client/busticket/auth_service/controller/TestController.java
+++ auth-service/src/main/java/com/client/busticket/auth_service/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.client.busticket.auth_service.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/status")
+    @PreAuthorize("hasAuthority('APP_READ')")
     public String getStatus(){
         String result = "New status is okay.";
         return result;
