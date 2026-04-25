aa88d778d0edcfde9c985f869932db3ddbe09303
PraveenMadavi <madavione@gmail.com>
2026-04-25 22:56:18 +0530

    oauth and properties added

---
+++ auth-service/src/main/java/com/client/busticket/auth_service/configuration/AppConfigs.java
@@ -0,0 +1,4 @@
+package com.client.busticket.auth_service.configuration;
+
+public class AppConfigs {
+}
---
+++ auth-service/src/main/java/com/client/busticket/auth_service/controller/JwksController.java
@@ -0,0 +1,4 @@
+package com.client.busticket.auth_service.controller;
+
+public class JwksController {
+}
