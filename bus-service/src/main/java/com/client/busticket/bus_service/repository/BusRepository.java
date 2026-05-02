7b68ed0df695a7c1d06d1d7b307c19d6928d70fd
PraveenMadavi <madavione@gmail.com>
2026-05-02 05:40:46 +0530

    BS-repos created.

---
+++ bus-service/src/main/java/com/client/busticket/bus_service/repository/BusRepository.java
@@ -0,0 +1,14 @@
+package com.client.busticket.bus_service.repository;
+
+import com.client.busticket.bus_service.entity.Bus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BusRepository extends JpaRepository<Bus, Long> {
+     Bus findByBusNumber(String busNumber);
+
+     List<Bus> findBySourceAndDestination(String source, String destination);
+}
---
+++ bus-service/src/main/java/com/client/busticket/bus_service/repository/TripRepository.java
@@ -0,0 +1,12 @@
+package com.client.busticket.bus_service.repository;
+
+import com.client.busticket.bus_service.entity.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TripRepository extends JpaRepository<Trip, Long> {
+     Trip findByBusId(Long busId);
+
+     Trip findBySourceAndDestination(String source, String destination);
+}
