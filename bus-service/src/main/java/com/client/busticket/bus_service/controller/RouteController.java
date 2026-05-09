80dab43c7b800032ac6fcc94bfed5d3ec096a412
PraveenMadavi <madavione@gmail.com>
2026-05-09 05:34:17 +0530

    Route and Trip: controller and services added.

---
+++ bus-service/src/main/java/com/client/busticket/bus_service/controller/RouteController.java
@@ -0,0 +1,4 @@
+package com.client.busticket.bus_service.controller;
+
+public class RouteController {
+}
---
+++ bus-service/src/main/java/com/client/busticket/bus_service/service/RouteService.java
@@ -0,0 +1,4 @@
+package com.client.busticket.bus_service.service;
+
+public class RouteService {
+}
