1cf8b59114f7c89c798dbdee792ad7a8c34e9cd0
PraveenMadavi <madavione@gmail.com>
2026-05-13 03:54:33 +0530

    feat(bus-service): add seat creation endpoint

--- bus-service/src/main/java/com/client/busticket/bus_service/controller/SeatController.java
+++ bus-service/src/main/java/com/client/busticket/bus_service/controller/SeatController.java
@@ -1,8 +1,14 @@
 package com.client.busticket.bus_service.controller;
 
+import com.client.busticket.bus_service.entity.Seat;
+import com.client.busticket.bus_service.records.SeatInfo;
 import com.client.busticket.bus_service.service.SeatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class SeatController {
 
     private final SeatService seatService;
+
+    @PostMapping("/seat")
+    public ResponseEntity<Seat> createSeat(@RequestParam SeatInfo seatInfo) {
+        Seat createdSeat = seatService.createSeat(seatInfo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdSeat);
+    }
 
 
 }
--- bus-service/src/main/java/com/client/busticket/bus_service/service/SeatService.java
+++ bus-service/src/main/java/com/client/busticket/bus_service/service/SeatService.java
@@ -1,5 +1,8 @@
 package com.client.busticket.bus_service.service;
 
+import com.client.busticket.bus_service.entity.Seat;
+import com.client.busticket.bus_service.records.SeatInfo;
+import com.client.busticket.bus_service.repository.BusRepository;
 import com.client.busticket.bus_service.repository.SeatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,12 @@ import org.springframework.stereotype.Service;
 public class SeatService {
 
     private final SeatRepository seatRepository;
+    private final BusRepository busRepository;
+
+    public Seat createSeat(SeatInfo seatInfo) {
+        Seat seat = new Seat();
+        busRepository.findById(seatInfo.busId()).ifPresent(seat::setBus);
+        seat.setSeatNumber(seatInfo.seatNumber());
+        return seatRepository.save(seat);
+    }
 }
