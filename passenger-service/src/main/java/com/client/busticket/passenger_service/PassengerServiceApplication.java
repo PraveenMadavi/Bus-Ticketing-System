f3e729e3ec2babf9c178cda2b864259fed6f9cd8
PraveenMadavi <madavione@gmail.com>
2026-05-14 04:14:28 +0530

    feat(passenger-service): add passenger service

---
+++ passenger-service/src/main/java/com/client/busticket/passenger_service/PassengerServiceApplication.java
@@ -0,0 +1,15 @@
+package com.client.busticket.passenger_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassengerServiceApplication.class, args);
+
+		System.out.println("Passenger Service is running...");
+	}
+
+}
