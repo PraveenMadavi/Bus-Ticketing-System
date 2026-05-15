95ab575fefa0252406fa485f4481bac32f24a9f7
PraveenMadavi <madavione@gmail.com>
2026-05-15 23:58:10 +0530

    feat(project): add eureka server in a project

---
+++ eureka-server/src/test/java/com/client/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
